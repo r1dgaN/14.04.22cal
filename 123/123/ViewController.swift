ba50b334814cd6b9e120f06bc05fc7486713964f
r1dgaN <103614065+r1dgaN@users.noreply.github.com>
2022-04-14 10:13:51 +0900

    Add files via upload

---
+++ 123/123/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  123
+//
+//  Created by Гость on 14.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
